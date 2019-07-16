70922133a0f4d31a4da5a32b81c77805245f018d
SamK <33693989+Haskool@users.noreply.github.com>
2019-07-16 03:29:05 +0100

    avoid namespace flooding by using pg not pygame

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from view import View
 from chess import Chess
-import pygame
+import pygame as pg
 from pygame.locals import *
 
 
@@ -10,19 +10,16 @@ class App:
         self._display_surf = None
         self._view = View()
 
-
     def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(
-            self._view.size, pygame.HWSURFACE | pygame.DOUBLEBUF
-        )
+        pg.init()
+        self._display_surf = pg.display.set_mode(self._view.size, pg.HWSURFACE | pg.DOUBLEBUF)
         self._view.on_init()
         self._running = True
         self._display_surf.blit(self._view._display_surf, (0, 0))
-        pygame.display.update()
+        pg.display.update()
 
     def on_event(self, event):
-        if event.type == pygame.QUIT:
+        if event.type == pg.QUIT:
             self._running = False
 
     def on_loop(self):
@@ -32,14 +29,14 @@ class App:
         pass
 
     def on_cleanup(self):
-        pygame.quit()
+        pg.quit()
 
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
 
         while self._running:
-            for event in pygame.event.get():
+            for event in pg.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
--- src/view.py
+++ src/view.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 
 
 class View:
@@ -6,24 +6,23 @@ class View:
         self._running = True
         self.size = self.width, self.height = 800, 800
         self.psize = self.pwidth, self.pheight = 100, 100
-        self._display_surf = pygame.Surface(self.size)
+        self._display_surf = pg.Surface(self.size)
 
     def on_init(self):
         # note that placement coords of an image is relative to top left corner
-
         # set board image
-        board = pygame.image.load("resources\\board.jpg").convert()
-        board = pygame.transform.scale(board, self.size)
+        board = pg.image.load("resources\\board.jpg").convert()
+        board = pg.transform.scale(board, self.size)
         self._display_surf.blit(board, (0, 0))
 
         # set major pieces
         piecePaths = ["r", "n", "b", "q", "k"]
         row = ["r", "n", "b", "q", "k", "b", "n", "r"]
         for piece in piecePaths:
-            bpieceImage = pygame.image.load("resources\\b{}.png".format(piece)).convert_alpha()
-            wpieceImage = pygame.image.load("resources\\w{}.png".format(piece)).convert_alpha()
-            bpiece = pygame.transform.scale(bpieceImage, self.psize)
-            wpiece = pygame.transform.scale(wpieceImage, self.psize)
+            bpieceImage = pg.image.load("resources\\b{}.png".format(piece)).convert_alpha()
+            wpieceImage = pg.image.load("resources\\w{}.png".format(piece)).convert_alpha()
+            bpiece = pg.transform.scale(bpieceImage, self.psize)
+            wpiece = pg.transform.scale(wpieceImage, self.psize)
             by = 0
             wy = self.height - self.pheight
             for i, square in enumerate(row):
@@ -33,10 +32,10 @@ class View:
                     self._display_surf.blit(wpiece, (x, wy))
 
         # set pawns
-        bpawnImage = pygame.image.load("resources\\bp.png").convert_alpha()
-        wpawnImage = pygame.image.load("resources\\wp.png").convert_alpha()
-        bpawn = pygame.transform.scale(bpawnImage, self.psize)
-        wpawn = pygame.transform.scale(wpawnImage, self.psize)
+        bpawnImage = pg.image.load("resources\\bp.png").convert_alpha()
+        wpawnImage = pg.image.load("resources\\wp.png").convert_alpha()
+        bpawn = pg.transform.scale(bpawnImage, self.psize)
+        wpawn = pg.transform.scale(wpawnImage, self.psize)
         for x in range(0, self.width, self.pwidth):
             by = self.pheight
             wy = self.height - 2 * self.pheight
